1640b410bafedd7b7ea60c9233cd29b907f03f62
pinstinct <gusals3407@gmail.com>
2024-01-02 15:40:08 +0900

    잠재적 서드 파티 옵션

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -1,8 +1,11 @@
 package com.example.demo1;
 
+import com.example.demo1.pojo.Droid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -12,4 +15,15 @@ public class Demo1Application {
 		SpringApplication.run(Demo1Application.class, args);
 	}
 
+	/*컴퍼넌트를 스프링빈으로 인스턴스화
+	* @Configuration 어노테이션이 달린 클래스 내에서 직접 또는 메타 어노테이션을 통해
+	* (여러 메타 어노테이션 중에서 @Configuration을 포함하는 메타 어노테이션이 바로 @SpringBootApplication
+	* 때문에 주로 빈 생성 메서드를 메인 클래스에 작성)
+	* @Bean 어노테이션이 달린 메소드를 생성하는 방법이 가장 적합
+	* */
+	@Bean
+	@ConfigurationProperties(prefix = "droid")
+	Droid createDroid() {
+		return new Droid();
+	}
 }
